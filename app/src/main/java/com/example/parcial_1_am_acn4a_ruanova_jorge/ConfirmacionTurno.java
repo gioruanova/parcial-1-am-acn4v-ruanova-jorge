305477884925f71e584e6e7e602f82d955504d97
Jorge Ruanova <jorge.r@saffire.com>
2024-09-17 22:51:00 -0300

    feat: margin agregado a inputs en pedido de turnos + imports edgetoedge removidos

--- app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/ConfirmacionTurno.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/ConfirmacionTurno.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityOptionsCompat;
 
