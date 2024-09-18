f546df864e4ef9f77a864b459e1697e42714cfbf
Jorge Ruanova <jorge.r@saffire.com>
2024-09-17 22:50:08 -0300

    feat: margin agregado a inputs en pedido de turnos

--- app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/ConfirmacionTurno.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/ConfirmacionTurno.java
@@ -19,7 +19,6 @@ public class ConfirmacionTurno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_confirmacion_turno);
 
 
--- app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/NuevoTurno.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_ruanova_jorge/NuevoTurno.java
@@ -31,7 +31,6 @@ public class NuevoTurno extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nuevo_turno);
 
 
